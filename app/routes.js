c547585a529228c65e74c02d4d3a41c130bf3c1a
Ty Fairclough <ty.fairclough@gmail.com>
2017-02-28 09:52:31 +0000

    adding v1 of finance screens

--- app/routes.js
+++ app/routes.js
@@ -18,6 +18,9 @@ module.exports = {
     app.get('/:p1/:p2/:p3/:p4/:p5', function (req, res) {
       res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5, {'sprint' : req.params.p2})
     })
+    app.get('/:p1/:p2/:p3/:p4/:p5/:p6', function (req, res) {
+      res.render(req.params.p1 + "/" + req.params.p2 + "/" + req.params.p3 + "/" + req.params.p4 + "/" + req.params.p5 + "/" + req.params.p6, {'sprint' : req.params.p2})
+    })
     app.get('/admin', function (req, res) {
             res.send('admin')
 //res.render(req.params.page)
